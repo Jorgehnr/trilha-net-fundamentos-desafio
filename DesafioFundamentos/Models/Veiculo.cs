e933206c365d397ff680e524262da6c997db4031
Jorge Honorio <86316909+Jorgehnr@users.noreply.github.com>
2025-07-07 17:27:39 -0300

    Adicionado Classe Veiculo
    Adição da Classe do modelo Veiculo com os atributos:
    tipoVeiculo;
    placaVeiculo:
    nomeMotorista;
    telefoneContato.

---
+++ DesafioFundamentos/Models/Veiculo.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DesafioFundamentos.Models
+{
+    public class Veiculo
+    {
+        public string tipoVeiculo = "";
+
+        public string placaVeiculo = "";
+
+        public string nomeMotorista = "";
+
+        public string telefoneContato = "";
+
+    }
+}
