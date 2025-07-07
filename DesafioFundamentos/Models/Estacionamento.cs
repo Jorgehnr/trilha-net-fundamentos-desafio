4f78762c8d6de9626b81f5444a2a7d8099fe2571
Jorge Honorio <86316909+Jorgehnr@users.noreply.github.com>
2025-07-07 17:32:03 -0300

    Atualizaçào da classe estacionamento
    Atualização da Classe estacionamento com os seguintes métodos:
    Adicionar veículo ao estacionamento com os dados do modelo do veículo.
    Remover veiculo ao estacionamento com confirmação de exclusão e calculo do valor total a pagar do período estacionado.
    Lista veículos do estacionamento com todos os dados dos veículos do estacionamento.

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -2,46 +2,121 @@ namespace DesafioFundamentos.Models
 {
     public class Estacionamento
     {
-        private decimal precoInicial = 0;
-        private decimal precoPorHora = 0;
-        private List<string> veiculos = new List<string>();
+        private decimal precoInicialCarro = 0;
+        private decimal precoPorHoraCarro = 0;
+        private decimal precoInicialMoto = 0;
+        private decimal precoPorHoraMoto = 0;
+        private List<Veiculo> listaVeiculos = new List<Veiculo>();
 
-        public Estacionamento(decimal precoInicial, decimal precoPorHora)
+        public Estacionamento(decimal precoInicialCarro, decimal precoPorHoraCarro, decimal precoInicialMoto, decimal precoPorHoraMoto)
         {
-            this.precoInicial = precoInicial;
-            this.precoPorHora = precoPorHora;
+            this.precoInicialCarro = precoInicialCarro;
+            this.precoPorHoraCarro = precoPorHoraCarro;
+            this.precoInicialMoto = precoInicialMoto;
+            this.precoPorHoraMoto = precoPorHoraMoto;
         }
 
         public void AdicionarVeiculo()
         {
-            // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
-            // *IMPLEMENTE AQUI*
+
+            Veiculo veiculoAdicionar = new Veiculo();
+            int menu = 1;
+
+            // Solicita o tipo do veiculor a ser adicionado ao estacionamento
+            while (menu == 1)
+            {
+                Console.WriteLine("Digite o tipo de veículo para estacionar: \n 1 - Carro \n 2 - Moto");
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        veiculoAdicionar.tipoVeiculo = "Carro";
+                        menu = 0;
+                        break;
+
+                    case "2":
+                        veiculoAdicionar.tipoVeiculo = "Moto";
+                        menu = 0;
+                        break;
+
+                    default:
+                        Console.WriteLine("Opção inválida");
+                        menu = 1;
+                        break;
+                }
+            }
+
+            // Solicita os dados do veiculo ser adicionado ao estacionamento
+            
             Console.WriteLine("Digite a placa do veículo para estacionar:");
+            veiculoAdicionar.placaVeiculo = Console.ReadLine().ToUpper();
+
+            Console.WriteLine("Digite o nome do motorista do veículo para estacionar:");
+            veiculoAdicionar.nomeMotorista = Console.ReadLine().ToUpper();
+
+            Console.WriteLine("Digite o telefone de contato:");
+            veiculoAdicionar.telefoneContato = Console.ReadLine();
+
+
+            listaVeiculos.Add(veiculoAdicionar);
+
         }
 
         public void RemoverVeiculo()
         {
-            Console.WriteLine("Digite a placa do veículo para remover:");
 
-            // Pedir para o usuário digitar a placa e armazenar na variável placa
-            // *IMPLEMENTE AQUI*
-            string placa = "";
+            Veiculo veiculoExcluir = new Veiculo();
+            decimal valorTotal = 0;
+
+            Console.WriteLine("Digite a placa do veículo para remover:");
+            string placa = Console.ReadLine().ToUpper();
+
 
             // Verifica se o veículo existe
-            if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
+            if (listaVeiculos.Any(x => x.placaVeiculo == placa))
             {
-                Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
+                // Busca os dados do veiculo
+                foreach (var item in listaVeiculos)
+                {
+                    if (item.placaVeiculo == placa)
+                    {
+                        veiculoExcluir = item;
+                    }
+                }
 
-                // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
-                // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
-                // *IMPLEMENTE AQUI*
-                int horas = 0;
-                decimal valorTotal = 0; 
+                // Exibir os dados do veiculo
+                Console.WriteLine($"Veiculo Tipo: {veiculoExcluir.tipoVeiculo} - Placa: {veiculoExcluir.placaVeiculo} - Motorista: {veiculoExcluir.nomeMotorista} - Contato: {veiculoExcluir.telefoneContato}");
 
-                // TODO: Remover a placa digitada da lista de veículos
-                // *IMPLEMENTE AQUI*
+                // Pedi confirmaçào para excluir o veiculo
+                Console.WriteLine($"Deseja realizar a saida do veículo {placa}?(1 - SIM, 2 - NÃO)");
+                int confirmacao = Convert.ToInt32(Console.ReadLine());
 
-                Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
+                if (confirmacao == 1)
+                {
+                    
+                    Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
+                    int horas = Convert.ToInt32(Console.ReadLine());
+
+
+                    // Realizar o calculo do valor a ser cobrado de acordo com o tipo de veiculo
+                    if (veiculoExcluir.tipoVeiculo == "Carro")
+                    {
+                        valorTotal = precoInicialCarro + precoPorHoraCarro * horas;
+                    }
+                    else
+                    {
+                        valorTotal = precoInicialMoto + precoPorHoraMoto * horas;
+                    }
+
+                    
+                    listaVeiculos.Remove(veiculoExcluir);
+
+                    Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
+                }
+                else
+                {
+                    Console.WriteLine($"Retirada do veículo {placa} cancelada.");
+                }
+                
             }
             else
             {
@@ -52,11 +127,16 @@ namespace DesafioFundamentos.Models
         public void ListarVeiculos()
         {
             // Verifica se há veículos no estacionamento
-            if (veiculos.Any())
+            if (listaVeiculos.Any())
             {
                 Console.WriteLine("Os veículos estacionados são:");
                 // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
-                // *IMPLEMENTE AQUI*
+                int contador = 1;
+                foreach (var item in listaVeiculos)
+                {
+                    Console.WriteLine($"Veiculo {contador} - Tipo: {item.tipoVeiculo} - Placa: {item.placaVeiculo} - Motorista: {item.nomeMotorista} - Contato: {item.telefoneContato}");
+                    contador++;
+                }
             }
             else
             {
