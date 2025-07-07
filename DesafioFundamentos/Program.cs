3441c5fa5dc1e5f1e5eed2c7ec9249eea55680c3
Jorge Honorio <86316909+Jorgehnr@users.noreply.github.com>
2025-07-07 17:25:28 -0300

    Atualização do Program.cs
    Adicionado as variáveis:
    precoHoraInicialCarro;
    precoPorHoraCarro;
    precoHoraInicialMoto;
    precoPorHoraMoto.

--- DesafioFundamentos/Program.cs
+++ DesafioFundamentos/Program.cs
@@ -3,18 +3,26 @@
 // Coloca o encoding para UTF8 para exibir acentuação
 Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-decimal precoInicial = 0;
-decimal precoPorHora = 0;
+decimal precoInicialCarro = 0;
+decimal precoPorHoraCarro = 0;
+decimal precoInicialMoto = 0;
+decimal precoPorHoraMoto = 0;
 
 Console.WriteLine("Seja bem vindo ao sistema de estacionamento!\n" +
-                  "Digite o preço inicial:");
-precoInicial = Convert.ToDecimal(Console.ReadLine());
+                  "Digite o preço inicial para Carro:");
+precoInicialCarro = Convert.ToDecimal(Console.ReadLine());
 
-Console.WriteLine("Agora digite o preço por hora:");
-precoPorHora = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("Agora digite o preço por hora para Carro:");
+precoPorHoraCarro = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine("Digite o preço inicial para Moto:");
+precoInicialMoto = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine("Agora digite o preço por hora para Motos:");
+precoPorHoraMoto = Convert.ToDecimal(Console.ReadLine());
 
 // Instancia a classe Estacionamento, já com os valores obtidos anteriormente
-Estacionamento es = new Estacionamento(precoInicial, precoPorHora);
+Estacionamento es = new Estacionamento(precoInicialCarro, precoPorHoraCarro, precoInicialMoto, precoPorHoraMoto);
 
 string opcao = string.Empty;
 bool exibirMenu = true;
